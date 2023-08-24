0abbd2138b1c542c500347a81c7e592b9b6c6f2e
Roberto Conrado <robertoconradorc@gmail.com>
2023-08-24 20:28:08 -0300

    aprendendo variaveis

---
+++ 2/main.go
@@ -0,0 +1,24 @@
+package main
+
+//Declaração de escopo global
+var (
+	b bool
+	c int
+	d string
+	e float32
+	f float64
+)
+
+func main() {
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+	println(f)
+}
+
+// Variável de escopo local
+func x() {
+	var a string
+	println(a)
+}
