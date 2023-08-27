e91aba8d30ae3d9d2ae07176cbca43f31d210ad9
Roberto Conrado <robertoconradorc@gmail.com>
2023-08-27 13:19:43 -0300

    maps.golang

---
+++ 7/maps.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	salarios := map[string]int{"roberto": 1000, "Vick": 2000, "Conrado": 3000}
+	// fmt.Println(salarios["roberto"])
+	delete(salarios, "roberto")
+	delete(salarios, "Vick")
+	salarios["Rocha"] = 5000
+	// fmt.Println(salarios["Rocha"])
+
+	for nome, salario := range salarios {
+		fmt.Printf("O salario de %s é %d\n", nome, salario)
+	}
+	
+	//blank identifier
+	for _, salario := range salarios {
+		fmt.Printf("O salario é %d\n", salario)
+	}
+}
+
+
+
