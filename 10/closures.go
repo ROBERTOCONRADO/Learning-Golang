36eea2ee5bd21264f77a9679d157a8cbff40a638
Roberto Conrado <robertoconradorc@gmail.com>
2023-08-30 11:44:22 -0300

    closures

--- 10/closures.go
+++ 10/closures.go
@@ -5,10 +5,7 @@ import (
 )
 
 func main() {
-	total := func() int {
-		return soma(1, 2, 5, 7, 9, 10, 13, 14, 15, 17, 19, 20, 23, 21, 25) * 2
-	}()
-	fmt.Println(total)
+	fmt.Println(soma(1, 2, 5, 7, 9, 10, 13, 14, 15, 17, 19, 20, 23, 21, 25))
 }
 
 func soma(numeros ...int) int {
